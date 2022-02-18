447826999ac538cc2c0600e60b81ff28e634cb30
GattoMac <traceformula2000@gmail.com>
2022-02-18 11:52:33 -0800

    Inference | output to file

--- hmmdecode.py
+++ hmmdecode.py
@@ -10,11 +10,19 @@ def main(test_file_path):
     outputs = []
     with open(test_file_path, 'r') as f:
         lines = f.read().split('\n')
-        for line in lines[:3]:
+        for line in lines:
             words = [w.strip() for w in line.split(' ')]
             pred_tags = model.viterbi(words)
-            print(words)
-            print(pred_tags)
+            # print(words)
+            # print(pred_tags)
+            sentence = [words[i]+'/'+pred_tags[i] for i in range(len(words))]
+            sentence = ' '.join(sentence)
+            outputs.append(sentence)
+
+    with open('hmmoutput.txt', 'w') as output_file:
+        for sentence in outputs:
+            output_file.write(sentence+'\n')
+        output_file.close()
 
 
 if __name__ == '__main__':
